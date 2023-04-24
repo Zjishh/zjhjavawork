608943b3263e75208ac781c31dc062dccfa9bbc4
38580 <385802188@qq.com>
2023-04-24 21:21:08 +0800

    W

--- src/zhengzebiaodashi/RegexTest1.java
+++ src/zhengzebiaodashi/RegexTest1.java
@@ -21,7 +21,7 @@ public class RegexTest1 {
     public static void main(String[] args) throws IOException {
         String QQ ="0385802188";
 
-
+//QQQQQQQQQQQQQQ
 
         System.out.println(QQ.matches("[1-9]\\d{5,19}"));
 
