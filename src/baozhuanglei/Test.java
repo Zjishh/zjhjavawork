b1679d6a0b9efcee9ff0452eed253c5538136688
38580 <385802188@qq.com>
2023-05-08 21:22:16 +0800

    5.8

--- src/baozhuanglei/Test.java
+++ src/baozhuanglei/Test.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  ****************************/
 public class Test {
 
-
+//1222222
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
