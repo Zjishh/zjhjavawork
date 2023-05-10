ae2a9acfb83cb4da7c5afea6e37ec021ff4c0076
38580 <385802188@qq.com>
2023-05-10 15:36:15 +0800

    5.9 jihe

---
+++ src/ListTest/Student.java
@@ -0,0 +1,74 @@
+package ListTest;
+
+import java.util.Objects;
+
+/****************************
+ * @project zjhjavawork
+ * @package ListTest
+ * @className Student
+ * @author Zjiah
+ * @date 2023/5/10 15:29
+ * @Description:   *
+ ****************************/
+public class Student {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Objects.equals(name, student.name);
+    }
+
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     *
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     *
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Student{name = " + name + ", age = " + age + "}";
+    }
+}
---
+++ src/suanfa/test1.java
@@ -0,0 +1,24 @@
+package suanfa;
+
+/****************************
+ * @project zjhjavawork
+ * @package suanfa
+ * @className test1
+ * @author Zjiah
+ * @date 2023/5/10 14:26
+ * @Description:   *
+ ****************************/
+public class test1 {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+
+
+
+}
