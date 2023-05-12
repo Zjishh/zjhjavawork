486131140fb8a982d7194daaf00570edb9053b1c
38580 <385802188@qq.com>
2023-05-12 15:51:22 +0800

    5.12

--- src/ListTest/Test1.java
+++ src/ListTest/Test1.java
@@ -2,6 +2,7 @@ package ListTest;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /****************************
  * @project zjhjavawork
@@ -35,6 +36,41 @@ public class Test1 {
         //需求：如果同姓名和同年龄，就认为是同一个学生。
         //所以，需要在自定义的Javabean类中，重写equals方法就可以了。
         System.out.println(coll.contains(s4));
+
+
+
+
+        ////-----------0--
+        /*
+            Collection系列集合三种通用的遍历方式：
+                1.迭代器遍历
+                2.增强for遍历
+                3.lambda表达式遍历
+
+
+             迭代器遍历相关的三个方法：
+                    Iterator<E> iterator()  ：获取一个迭代器对象
+                    boolean hasNext()       ：判断当前指向的位置是否有元素
+                    E next()                ：获取当前指向的元素并移动指针
+        */
+
+        //1.创建集合并添加元素
+        Collection<String> colll = new ArrayList<>();
+        colll.add("aaa");
+        colll.add("bbb");
+        colll.add("ccc");
+        colll.add("ddd");
+//999
+        //2.获取迭代器对象
+        //迭代器就好比是一个箭头，默认指向集合的0索引处
+        Iterator<String> it = colll.iterator();
+        //3.利用循环不断的去获取集合中的每一个元素
+        while(it.hasNext()){
+            //4.next方法的两件事情：获取元素并移动指针
+            String str = it.next();
+            System.out.println(str);
+        }
+
     }
 
 
