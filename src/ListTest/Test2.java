0822961fc18838c2c6db830aa76edf2dffec94f0
38580 <385802188@qq.com>
2023-05-15 21:52:25 +0800

    ww

---
+++ src/ListTest/Test2.java
@@ -0,0 +1,30 @@
+package ListTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/****************************
+ * @project zjhjavawork
+ * @package ListTest
+ * @className Test2
+ * @author Zjiah
+ * @date 2023/5/15 21:27
+ * @Description:   *
+ ****************************/
+public class Test2 {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+
+        list.add(1,"wqe");
+        String remove = list.remove(1);
+        System.out.println(remove);
+
+        System.out.println(list);
+
+    }
+}
